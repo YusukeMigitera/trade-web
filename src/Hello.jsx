24b6963cea07402da8392fd731e6f67cfd145c4c
yusukemigitera <ymigitera@gmail.com>
2022-05-10 23:58:52 +0900

    add: pages

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -1,4 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
-const Hello = () => <h1>こんにちは！</h1>;
+const Hello = () => (
+    <main>
+        <h1>ハッカーと画家</h1>
+        <Link to="page1">
+            <h2>どうしてオタクはモテないか</h2>
+        </Link>
+        <Link to="page2">
+            <h2>ハッカーと画家</h2>
+        </Link>
+        <Link to="page3">
+            <h2>口にできないこと</h2>
+        </Link>
+        <a href="https://gist.github.com/sifue/b6506ea6b3f3d3a46a0c3bb885cd5ddf">
+            <p>無料で読めるポール・グレアムの「ハッカーと画家」+αの日本語訳のみのまとめ</p>
+        </a>
+    </main>);
 export default Hello;
--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Hello from "./Hello";
+import Page1 from "./pages/Page1";
+import Page2 from "./pages/Page2";
+import Page3 from "./pages/Page3";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Hello />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+        <Route path="page1" element={<Page1 />} />
+        <Route path="page2" element={<Page2 />} />
+        <Route path="page3" element={<Page3 />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("app")
 );
