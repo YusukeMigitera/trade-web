a0e58e285fc4894611df3b3f56a9f975d0f9cca9
yusukemigitera <ymigitera@gmail.com>
2022-07-13 01:50:45 +0900

    ts最小設定

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Hello from './Hello';
 import Page1 from './pages/Page1';
 import Page2 from './pages/Page2';
@@ -9,12 +9,10 @@ import Page3 from './pages/Page3';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-        <Route path="page1" element={<Page1 />} />
-        <Route path="page2" element={<Page2 />} />
-        <Route path="page3" element={<Page3 />} />
-      </Routes>
+      <Route path="/" children={<Hello />} />
+      <Route path="page1" children={<Page1 />} />
+      <Route path="page2" children={<Page2 />} />
+      <Route path="page3" children={<Page3 />} />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('app')
